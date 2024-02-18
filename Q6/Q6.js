1bdbc3c1f47be40c1fd8d76093e3d673d1223d4d
saqibsattar07 <159882823+saqibsattar07@users.noreply.github.com>
2024-02-18 13:46:12 +0500

    Add files via upload

---
+++ Q6/Q6.js
@@ -0,0 +1,7 @@
+"use strict";
+//Question NO: 06
+//Stripping Names: Store a person’s name, and include some whitespace characters at the beginning and end of the name. Make sure you use each character combination, "\t" and "\n", at least once. Print the name once, so the whitespace around the name is displayed. Then print the name after striping the white spaces.
+const nameWithWhitespace = "\t\n   Saqib Sattar   \t\n";
+//console.log("Name with whitespace:", nameWithWhitespace);
+const strippedName = nameWithWhitespace.trim();
+//console.log("Stripped name:", strippedName);
