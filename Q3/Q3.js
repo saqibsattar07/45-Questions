55d5adcd395d14d830d1a17265297ca92e2df359
saqibsattar07 <159882823+saqibsattar07@users.noreply.github.com>
2024-02-18 13:43:08 +0500

    Add files via upload

---
+++ Q3/Q3.js
@@ -0,0 +1,7 @@
+"use strict";
+//Question NO: 03
+//Name Cases: Store a person’s name in a variable, and then print that person’s name in lowercase, uppercase, and titlecase.
+let firstName = "Saqib Sattar";
+//console.log(firstName.toLocaleLowerCase());
+//console.log(firstName.toLocaleUpperCase());
+//console.log(firstName.charAt(0).toUpperCase() + firstName.slice(1));
---
+++ Q3/Q3.ts
@@ -0,0 +1,8 @@
+//Question NO: 03
+//Name Cases: Store a person’s name in a variable, and then print that person’s name in lowercase, uppercase, and titlecase.
+
+let firstName: string= "Saqib Sattar";
+
+//console.log(firstName.toLocaleLowerCase());
+//console.log(firstName.toLocaleUpperCase());
+//console.log(firstName.charAt(0).toUpperCase() + firstName.slice(1));
