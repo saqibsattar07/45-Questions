6e52f3af63449d33011bc759a24c4eac5f3192d4
saqibsattar07 <159882823+saqibsattar07@users.noreply.github.com>
2024-02-18 13:46:48 +0500

    Add files via upload

---
+++ Q7/Q7.ts
@@ -0,0 +1,10 @@
+//Question NO: 07
+//Number Eight: Write addition, subtraction, multiplication, and division operations that each result in the number 8. Be sure to enclose your operations in print statements to see the results.
+
+//console.log("Addition:", 5 + 3);
+
+//console.log("Subtraction:", 10 - 2);
+
+//console.log("Multiplication:", 4 * 2);
+
+//console.log("Division:", 16 / 2);
