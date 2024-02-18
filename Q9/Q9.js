98f8718360a053e0386b7fab7fd1eb0674124508
saqibsattar07 <159882823+saqibsattar07@users.noreply.github.com>
2024-02-18 13:48:57 +0500

    Add files via upload

---
+++ Q9/Q9.js
@@ -0,0 +1,6 @@
+"use strict";
+//Question NO: 09
+//Favorite Number: Store your favorite number in a variable. Then, using that variable, create a message that reveals your favorite number. Print that message.
+const favoriteNumber = 7;
+const message = `My favorite number is ${favoriteNumber}.`;
+//console.log(message);
