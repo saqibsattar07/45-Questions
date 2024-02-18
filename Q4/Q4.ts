5d22dd6e1ccb3241f8a15cf8ba836b3cb53847e2
saqibsattar07 <159882823+saqibsattar07@users.noreply.github.com>
2024-02-18 13:44:10 +0500

    Add files via upload

---
+++ Q4/Q4.ts
@@ -0,0 +1,9 @@
+//Question NO: 04 
+//Famous Quote: Find a quote from a famous person you admire. Print the quote and the name of its author. Your output should look something like the following, including the quotation marks:
+//Albert Einstein once said, “A person who never made a mistake never tried anything new.”
+
+
+const quote = "A person who never made a mistake never tried anything new.";
+const author = "Albert Einstein";
+
+//console.log(`${author} once said, "${quote}"`);
